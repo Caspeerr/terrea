7d9baad2f6fcd722f12bb817e910e8898b00f78c
Aayushrestha-pixel <ayushrestha7007@gmail.com>
2026-07-12 11:46:59 +0545

    fix: enable force_destroy on S3 bucket so terraform destroy works on non-empty bucket

--- frontend/main.tf
+++ frontend/main.tf
@@ -3,7 +3,8 @@
 ########################################
 
 resource "aws_s3_bucket" "frontend" {
-  bucket = var.frontend_bucket_name
+  bucket         = var.frontend_bucket_name
+  force_destroy = true
 
   tags = {
     Project = "CONC"
